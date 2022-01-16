32dc6b1166017746edb61040258f9307b3e16784
Ian <iansedano@gmail.com>
2022-01-16 15:17:43 +0100

    adding pytest

--- shell.py
+++ shell.py
@@ -1,4 +1,3 @@
-import os
 import platform
 import subprocess
 
@@ -21,53 +20,66 @@ def init_shell():
 		
 class Shell(ABC):
 	@abstractmethod
-	def ignore_folders(self, paths: list(Path)):
+	def ignore_folders(self, paths: list[Path]):
 		pass
 
 class Pwsh_shell(Shell):
-	def run(self, cmd):
+	def _run(self, cmd):
 		completed_process = subprocess.run(["pwsh", "-Command", cmd], capture_output=True)
 		return completed_process
 	
-	def run_with_output(self, cmd):
-		process = self.run(cmd)
+	def _run_with_output(self, cmd):
+		process = self._run(cmd)
 		print(process.stdout.decode(), process.stderr.decode())
 		return process
 	
-	def send_ignore_command(self, path: Path):
+	def _send_ignore_command(self, path: Path):
 		command = f"Set-Content -Path '{str(path)}' -Stream com.dropbox.ignored -Value 1"
-		return self.run_with_output(command)
+		return self._run_with_output(command)
 		
-	def get_ignored_value(self, path: Path):
+	def _get_ignored_value(self, path: Path):
 		command = f"Get-Content -Path '{str(path)}' -Stream com.dropbox.ignored"
-		return self.run_with_output(command)
+		return self._run_with_output(command)
 	
-	def ignore_folders(self, paths: list(Path)):
+	def ignore_folders(self, paths: list[Path]):
 		for path in paths:
-			self.send_ignore_command(path)
-
-class Bash_shell(Shell):
-
-	def run(self, cmd):
-		completed_process = subprocess.run([cmd], capture_output=True)
-		return completed_process
-	
-	def run_with_output(self, cmd):
-		process = self.run(cmd)
-		print(process.stdout.decode(), process.stderr.decode())
-		return process
-	
-	def get_exclude_list(self):
-		exclude_list = self.run_with_output("dropbox exclude list")
-	
-	def ignore_folders(self, paths: list(Path)):
-		exclude_list = self.get_exclude_list()
-		print(exclude_list)
-		command = f"drobox exclude {' '.join([str(path) for path in paths])}"
-		return self.run_with_output(command)
+			self._send_ignore_command(path)
 	
 	# "list" prints a list of directories currently excluded from syncing.
 	# "add" adds one or more directories to the exclusion list, then
 	# resynchronizes Dropbox.
 	# "remove" removes one or more directories from the exclusion list, then
 	# resynchronizes Dropbox.
+
+class Bash_shell(Shell):
+	
+	# https://help.dropbox.com/files-folders/restore-delete/ignored-files
+
+	def _run(self, cmd):
+		completed_process = subprocess.run([cmd], capture_output=True)
+		return completed_process
+	
+	def _run_with_output(self, cmd):
+		process = self._run(cmd)
+		print(process.stdout.decode(), process.stderr.decode())
+		return process
+	
+	def _init_exclude_list(self):
+		exclude_list = self._run_with_output("dropbox exclude list")
+		"""
+		Excluded: 
+		../../../Dropbox/0 JPA
+		../../../Dropbox/0 Music
+		../../../Dropbox/0 Projects
+		"""
+		
+	def _send_ignore_command(self):	
+		pass
+	
+	def ignore_folders(self, paths: list[Path]):
+		exclude_list = self._get_exclude_list()
+		print(exclude_list)
+		command = f"drobox exclude {' '.join([str(path) for path in paths])}"
+		return self._run_with_output(command)
+	
+	
--- test_suite.py
+++ test_suite.py
@@ -5,4 +5,9 @@ import folders_to_ignore
 
 def test_should_instantiate_pwsh_shell():
 	shell.Pwsh_shell()
+
+def test_should_instantiate_bash_shell():
 	shell.Bash_shell()
+	
+
+	
