3b3707032187586cfb64999a4822fd52b12b38b5
Ian Currie <iansedano@gmail.com>
2024-09-13 15:41:12 +0200

    Add Zsh shell runner for Mac

--- dropboxignore/shell.py
+++ dropboxignore/shell.py
@@ -19,8 +19,7 @@ def init_shell():
     elif system == "Windows":
         return Pwsh_shell()
     elif system == "Darwin":
-        return Bash_shell()
-
+        return Zsh_shell()
 
 class Shell(ABC):
     """Interface for shells"""
@@ -58,6 +57,8 @@ class Pwsh_shell(Shell):
 
 
 class Bash_shell(Shell):
+    """Bash ignore runner"""
+    
     @staticmethod
     def _make_string_path_list(paths: list[Path]):
         """Joins list of paths into one long string to pass into bash"""
@@ -84,6 +85,39 @@ class Bash_shell(Shell):
         path_list = self._make_string_path_list(paths)
         command = (
             f"for f in '{path_list}'\ndo\nattr -s com.dropbox.ignored -V 1 $f\ndone"
+        )
+        run(["bash", "-c", command], check=True)
+        print("Done!")
+
+class Zsh_shell(Shell):
+    """Zsh ignore runner"""
+    
+    @staticmethod
+    def _make_string_path_list(paths: list[Path]):
+        """Joins list of paths into one long string to pass into bash"""
+        return "' '".join([str(path).replace("'", "\\'") for path in paths])
+
+    def get_ignored_status(self, paths: list[Path]):
+        """Query the Dropbox ignored status"""
+        path_list = self._make_string_path_list(paths)
+
+        command = textwrap.dedent(
+            f"""\
+            for f in '{path_list}'
+            do
+                if  (xattr -q -g com.dropbox.ignored $f)
+                then
+                    echo "$f"
+                fi
+            done"""
+        )
+        run(["sh", "-c", command])
+
+    def ignore_folders(self, paths: list[Path]):
+        """Sends the ignore command to bash"""
+        path_list = self._make_string_path_list(paths)
+        command = (
+            f"for f in '{path_list}'\ndo\nxattr -s com.dropbox.ignored -V 1 $f\ndone"
         )
         run(["bash", "-c", command], check=True)
         print("Done!")
